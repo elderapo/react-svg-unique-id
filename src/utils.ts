be527137b4fe0f1321859e7f89f29c8bf43bd340
Tomasz Martyński <tomek@martynski.pl>
2019-05-04 14:08:59 +0200

    fix: fix imports

--- src/utils.ts
+++ src/utils.ts
@@ -1,6 +1,5 @@
-import { deepMap as deepMapType } from 'react-children-utilities'
 // @ts-ignore
-import ReactChildrenUtilities from 'react-children-utilities'
+import ReactChildrenUtilities, { deepMap as deepMapType } from 'react-children-utilities'
 
 export const reactRecursiveChildrenMap = ReactChildrenUtilities.deepMap.bind(
   ReactChildrenUtilities
